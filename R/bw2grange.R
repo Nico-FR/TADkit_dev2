dc719f7cd9ea6fa0d372f52a8673d1b296babbec
nmary <n.mary@envt.fr>
2023-02-24 13:49:55 +0100

    bw2grange: function to translate bigwig path to GRange object

---
+++ R/bw2grange.R
@@ -0,0 +1,27 @@
+#' Convert bigwig file to GRange object
+#'
+#' @description From the full path of a bigwig files, return a GRanges object. The bin width of the GRnages can be specify with bin.width parameter.
+#'
+#'
+#' @param bigwig.path path of bigwig file.
+#' @param bin.width Bin width. Default is NULL to return the bin size as it is in the bigwig file.
+#'
+#' @return GRanges object
+#' @import GenomicRanges
+#' @import GenomeInfoDb
+#' @import rtracklayer
+#' @export
+#'
+bw2grange <- function(bigwig.path, bin.width = NULL) {
+
+  gr = rtracklayer::import.bw(bigwig.path, as = "GRanges")
+
+  if (is.null(bin.width)) {return(gr)
+  } else {
+
+    bin.gr <- GenomicRanges::tileGenome(GenomeInfoDb::seqlengths(gr),
+                                      tilewidth = bin.width, cut.last.tile.in.chrom = TRUE)
+
+    return(rtracklayer::import.bw(bigwig.path, which = bin.gr, as = "GRanges"))
+  }
+}
