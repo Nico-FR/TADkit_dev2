316990df0bca1f06dcf954566a92843a44be75f9
nmary <n.mary@envt.fr>
2023-05-22 13:17:09 +0200

    MATplot: dgCMatrix -> CsparseMatrix

--- R/MATplot.R
+++ R/MATplot.R
@@ -73,7 +73,7 @@ MATplot <- function(matrix, start, stop, bin.width, log2 = T, scale.colors = "H"
   if (log2 == T) {mat@x = log2(mat@x)}
 
   #melt matrix
-  upper_mat = Matrix::summary(Matrix::triu(as(mat, "dgCMatrix"), 1))
+  upper_mat = Matrix::summary(Matrix::triu(as(mat, "CsparseMatrix"), 1))
   diag_mat = data.frame(i = 1:nrow(mat), j = 1:nrow(mat), x = Matrix::diag(mat))
   lower_mat = data.frame(i = upper_mat$j, j = upper_mat$i, x = upper_mat$x) #Matrix::summary(Matrix::tril(BiocGenerics::t(mat), -1))
 
