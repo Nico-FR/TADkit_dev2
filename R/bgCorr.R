830b6f6a7b3f6ba781277a0b763a78aa74d84e3c
nmary <n.mary@envt.fr>
2023-07-20 13:43:37 +0200

    bgCorr: 1st commit

---
+++ R/bgCorr.R
@@ -0,0 +1,65 @@
+#' @title Correlation between bedgraph scores
+#'
+#' @description Computes the correlations between bin scores (e.g. between insulation scores...) from 2 or more individuals.
+#'
+#'
+#' @param bedgraph.lst list of `data.frame`, `GRanges` or full path of the bedgraph files (data frame without header and 4 columns tab separated) containing a score for each bin.
+#' @param method a character string indicating which correlation coefficient is to be computed. One of "pearson" (default), "kendall", or "spearman": can be abbreviated.
+#' @param rm_chr chromosome to filter, default = "X".
+#'
+#' @return matrix with correlation values
+#'
+#' @import GenomicRanges
+#' @importFrom utils read.table
+#' @importFrom dplyr select full_join filter
+#' @examples
+#' bgCorr(list(ind1 = IS_1_10kb.bedgraph, ind2 = IS_2_10kb.bedgraph))
+#'
+#' @export
+#'
+bgCorr <- function(bedgraph.lst, method = "pearson", rm_chr = "X") {
+
+  #Stop if bedgraphPath is not a list
+  if (!is.list(bedgraph.lst)) {
+    stop("bedgraph.lst must be a list")
+  }
+
+  #read bedgraph datas
+  ##if dataframe
+  if (is.data.frame(bedgraph.lst[[1]])) {
+    data1 =  base::lapply(bedgraph.lst, function(bg){
+      bg[,1:4]})
+  }
+  ##if path
+  if (is.character(bedgraph.lst[[1]])) {
+    data1 = base::lapply(bedgraph.lst, function(bg){
+      utils::read.table(bg, header = FALSE, sep = "\t")[,1:4]})
+  }
+  ##if GRanges
+  if (inherits(bedgraph.lst[[1]], "GRanges")) {
+    data1 = base::lapply(bedgraph.lst, function(bg){
+      as.data.frame(bg) %>% dplyr::select("seqnames", "start", "end", "V4")})
+  }
+
+  #add name to V4
+  for (c in names(data1)) {
+    names(data1[[c]]) = c("seqnames", "start", "end", c)
+  }
+
+  #merge bedgraphs
+  data2 = data1 %>% Reduce(function(...) dplyr::full_join(...), .) %>% dplyr::filter(!seqnames %in% rm_chr)
+
+  #create matrix
+  mat=matrix(nrow = length(bedgraph.lst), ncol = length(bedgraph.lst))
+  colnames(mat) = rownames(mat) = names(bedgraph.lst)
+
+  for (ind1 in names(bedgraph.lst)) {
+    for (ind2 in names(bedgraph.lst)) {
+      if (ind1 == ind2) {next}
+      mat[ind1, ind2] = cor(data2[, ind1], data2[, ind2], use = "complete.obs", method = method)
+    }
+  }
+  diag(mat) = 1
+
+  return(mat)
+}
